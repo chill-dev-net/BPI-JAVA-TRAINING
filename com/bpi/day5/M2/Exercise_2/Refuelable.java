78664b59693be5bd2a57911ea0fee88887e290c0
chill-dev-net <52799003+chill-dev-net@users.noreply.github.com>
2025-12-04 23:26:35 +0800

    Delete com/bpi/day5 directory

--- com/bpi/day5/M2/Exercise_2/Refuelable.java
+++
@@ -1,5 +0,0 @@
-package com.bpi.day5.M2.Exercise_2;
-
-interface Refuelable {
-	void refuel();
-}
--- com/bpi/day5/M2/SeatWork_3/Executable.java
+++
@@ -1,7 +0,0 @@
-package com.bpi.day5.M2.SeatWork_3;
-
-public interface Executable {
-	void run();
-
-	void stop();
-}
