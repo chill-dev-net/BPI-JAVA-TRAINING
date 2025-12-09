f15ebd9df27f81b737638cde2456ef3909fc085a
Churchill L. Villanueva <clvillanueva@headoffice.corp.bpi.com.ph>
2025-12-09 08:46:54 +0800

    Adding SW3

---
+++ day5_M2_SeatWork_3/Executable.java
@@ -0,0 +1,7 @@
+package com.bpi.day5_M2_SeatWork_3;
+
+public interface Executable {
+	void run();
+
+	void stop();
+}
---
+++ day5_M2_SeatWork_3/SeatWork_3.java
@@ -0,0 +1,22 @@
+package com.bpi.day5_M2_SeatWork_3;
+
+public class SeatWork_3 {
+	public static void main(String[] args) {
+		Executable excel = new MSExcel();
+		Executable word = new MSWord();
+
+		runProgram(excel);
+		runProgram(word);
+
+		stopProgram(excel);
+		stopProgram(word);
+	}
+
+	private static void runProgram(Executable executableProgram) {
+		executableProgram.run();
+	}
+
+	private static void stopProgram(Executable executableProgram) {
+		executableProgram.stop();
+	}
+}
